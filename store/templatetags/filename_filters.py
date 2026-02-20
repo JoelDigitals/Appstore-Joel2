60702bf3a29d17d4ddaaa83a416b318e5f76bf0d
Joel <joel-digitals@web.de>
2026-02-20 22:34:33 +0100

    1.1.0.0. hFx2.0

--- store/templatetags/filename_filters.py
+++ store/templatetags/filename_filters.py
@@ -4,5 +4,7 @@ from django import template
 register = template.Library()
 
 @register.filter
-def basename(value):
-    return os.path.basename(value)
+def filename(value):
+    if value:
+        return os.path.basename(value)
+    return ""
